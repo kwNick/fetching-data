991286dca65295a67ddf9058fd3c11882d29c974
Nickolas Piraino <kwnp17c@gmail.com>
2024-04-29 20:17:47 -0400

    creating layout and nav

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='flex justify-center items-center '>
+            <div className='text-center '>
+                <p className='group relative overflow-hidden'>
+                    Fetching Data!!
+                    <span className='absolute w-full h-[5%]] border-b border-b-[#9932CC] bottom-0 -left-[100%] group-hover:translate-x-[100%] duration-300' />
+                </p>
+                <p>
+                    Login
+                </p>
+            </div>
+        </div >
+    )
+}
+
+export default Header
